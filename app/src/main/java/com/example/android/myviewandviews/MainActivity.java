9515e0c6e99e70b96d7ad696dcdc943549a9d962
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-02-15 10:57:21 +0700

    Implement View and ViewGroup

--- app/src/main/java/com/example/android/myviewandviews/MainActivity.java
+++ app/src/main/java/com/example/android/myviewandviews/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Cari jika action bar exist, jika iya maka set title nya
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Google Pixel");
+        }
     }
 }
